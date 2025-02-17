c31378bfc99f1bbb2cc36133b3c43f02c29beca5
Darley <159792141+darleynartey@users.noreply.github.com>
2025-02-17 09:50:13 +0000

    add firebase details to login file

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,20 +1,23 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 //import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 const Login = () => {
   var firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
+    apiKey: "AIzaSyC5VSSVNKZg9rYs6hQ4MdeURpX067QQ0-k",
+    authDomain: "hub-meal.firebaseapp.com",
     // The value of `databaseURL` depends on the location of the database
     databaseURL: "https://DATABASE_NAME.firebaseio.com",
-    projectId: "PROJECT_ID",
+    projectId: "hub-meal",
     // The value of `storageBucket` depends on when you provisioned your default bucket (learn more)
-    storageBucket: "PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
+    storageBucket: "hub-meal.firebasestorage.app",
+    messagingSenderId: "536258794391",
+    appId: "1:536258794391:web:202dcc50b6c0129c562ad2",
     // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
     measurementId: "G-MEASUREMENT_ID",
   };
   const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app)
 
   return (
     <>
