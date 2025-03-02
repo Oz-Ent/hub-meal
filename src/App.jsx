b2a24a254cdc2e8dfb0c0f4ae72cfcb067075371
Kwakye Osafo-Bediako <kwakye.osafo-bediako@heidelbergmaterials.com>
2025-03-02 11:13:28 +0000

    protected route fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,20 @@
 import "./App.css";
 import Login from "./components/Login";
 import Sorter from "./sorter";
-// import { useAuth } from "./components/authContext";
-// import { Outlet, Router, Routes } from "react-router-dom";
-// import SpreadsheetData from "./Spreadsheet";
-
-// // 1TXVYdlIKY9NLZZJAtLLLUcrdd6jwPZXZlA5QIPoeQqg
-
-// // 12345689abcdfgd
-
-// function App() {
-//   return (
-//     <>
-//       {/* <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p> */}
-//       {/* <SpreadsheetData /> */}
-//       {/* <Sorter /> */}
-//       {/* <Login /> */}
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<Login/>}/>
-//           <Route path="/recover" element={<Register/>}/>
-//           <Route path="/sort" element={<Sorter/>}/>
-//         </Routes>
-//       </Router>
-//       <Outlet/>
-//     </>
-//   );
-// }
-
-// export default App;
+import ProtectedRoute from "./protectedRoute";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Recover from "./components/Recover";
-// import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/recover" element={<Recover />} />
+      <Routes>
+        <Route element={<ProtectedRoute />}>
           <Route path="/sort" element={<Sorter />} />
-        </Routes>
+        </Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/recover" element={<Recover />} />
+      </Routes>
     </Router>
   );
 }
